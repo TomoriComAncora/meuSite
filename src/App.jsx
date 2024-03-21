cadd30f50d9882c206c7296fa462858ac17bb6da
TomoriComAncora <lucas.fernando@ufms.br>
2024-03-20 21:24:30 -0400

    aprendendo mais sobre hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,31 @@
-import React, { Component } from "react";
-import Biscoito from "./assets/Biscoito.png";
-import "./styles.css";
+import React, { useState } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      textoFrase: "",
-    };
+function App() {
+  const [tarefas, setTarefas] = useState(["Pagar Luz", "Limpar casa"]);
+  const [input, setInput] = useState("");
 
-    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+  const handleAdd = () => {
+    setTarefas([...tarefas, input]);
+    setInput("");
+  };
 
-    this.frases = [
-      "Siga os bons e aprenda com eles.",
-      "O bom-senso vale mais do que muito conhecimento.",
-      "O riso é a menor distância entre duas pessoas.",
-      "Deixe de lado as preocupações e seja feliz.",
-      "Realize o óbvio, pense no improvável e conquiste o impossível.",
-      "Acredite em milagres, mas não dependa deles.",
-      "A maior barreira para o sucesso é o medo do fracasso.",
-    ];
-  }
-
-  quebraBiscoito() {
-    let state = this.state;
-    let numAleatorio = Math.floor(Math.random() * this.frases.length);
-
-    state.textoFrase ='"' +  this.frases[numAleatorio] + '"';
-    this.setState(state);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <img src={Biscoito} alt="biscoito" className="img" />
-        <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito} />
-        <h1 className="textoFrase">{this.state.textoFrase}</h1>
-      </div>
-    );
-  }
-}
-
-class Botao extends Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      Hooks
+      <ul>
+        {tarefas.map((tarefa, e) => (
+          <li key={e}>{tarefa}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Add Tarefa"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleAdd}>Adicionar</button>
+    </div>
+  );
 }
 
 export default App;
